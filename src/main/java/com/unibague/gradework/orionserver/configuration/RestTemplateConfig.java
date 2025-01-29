cff766fb37e3e64f4167b3d07a45660749a03930
Sebastian Murillo <sebasty06@outlook.com>
2025-01-29 13:05:05 -0500

    RestTemplate documentation.

--- src/main/java/com/unibague/gradework/orionserver/configuration/RestTemplateConfig.java
+++ src/main/java/com/unibague/gradework/orionserver/configuration/RestTemplateConfig.java
@@ -4,9 +4,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * Configuration class for defining application-wide beans.
+ *
+ * This class is responsible for creating and providing a {@link RestTemplate} bean,
+ * which is used for making HTTP requests to external services within the application.
+ *
+ * Annotations:
+ * - {@link Configuration}: Marks this class as a configuration class, allowing Spring
+ *   to detect and register its beans.
+ */
 @Configuration
 public class RestTemplateConfig {
 
+    /**
+     * Creates and registers a {@link RestTemplate} bean.
+     *
+     * The {@link RestTemplate} is a Spring utility class used to perform HTTP
+     * requests and communicate with external APIs or microservices.
+     *
+     * @return a new instance of {@link RestTemplate}.
+     */
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
